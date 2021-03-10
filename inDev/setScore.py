5a294e7139c3831475409cc9ba164d35093990ee
Anja Cavalieri <anja.cavalieri@students.fhnw.ch>
2021-03-10 08:54:56 +0100

    Autor

---
+++ inDev/setScore.py
@@ -0,0 +1,17 @@
+# author: Anja
+# General conventions:
+#       - functions, variables: firstLetterLowerCase
+#       - class: FirstLetterCapital
+#       - constant: CAPITAL
+#############################################################################
+
+'''
+Doc: your remarks, ideas
+'''
+
+# POST: -
+# PAST: -
+
+def setScore(self):
+    #TODO: calculates the score from path the player walked 
+    return
